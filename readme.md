e54d3ab6fb8a039590b8bf4326de9e650d74f102
weykon <weykon@qq.com>
2024-07-03 17:30:28 +0800

    init

---
+++ readme.md
@@ -0,0 +1,11 @@
+# operation system
+https://os.phil-opp.com/zh-CN/minimal-rust-kernel/ 
+## description 
+code practice of operation system learning
+
+## wynOS 
+
+1. macos, linux, windows 在rustup的target build中，需要不同的参数来满足打开入口执行的选项。
+2. 使用BIOS较为简单的实践。BIOS是在存储上512字节长度的程序片段。512字节为基本，第一段他们觉第一阶段，后面的更成更大的可以叫第二阶段，像火箭推进器。
+
+3. 定义target json，作为编译目标配置。
---
+++ src/main.rs
@@ -0,0 +1,23 @@
+#![no_std]
+#![no_main]
+use core::panic::PanicInfo;
+/// 这个函数将在 panic 时被调用
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    loop {}
+}
+
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    // as包含转化操作，对这个裸指针可进行mut操作
+    let vga_buffer = 0xb8000 as *mut u8;
+
+    for (i, &byte) in HELLO.iter().enumerate() {
+        unsafe {
+            *vga_buffer.offset(i as isize * 2) = byte;
+            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
+        }
+    }
+
+    loop {}
+}
